26e86eba96341fcedf2bf7dfb0fa41ce970a6834
Kylan Byrd <AvianAnalyst@gmail.com>
2020-04-17 23:04:21 -0700

    Refactor of test to ensure testing proper response
    Previously it was testing for a valid result from the requests.get() function, but not ensuring itw as gathering search results. Updating it to check for a key in the results dictionary fixes this issue, as 'items' doesn't exist in a specific item result.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 import os
 import requests
+import json
 
 load_dotenv()
 SECRET = os.getenv('KEY')
 
 
 def search(query: str):
-    return requests.get('https://www.googleapis.com/books/v1/volumes', {'q': query})
+    response = requests.get('https://www.googleapis.com/books/v1/volumes', {'q': query})
+    return json.loads(response.text)
--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 import main
 
 
-def test_search_returns_200_status_code():
+def test_search_gets_valid_response_with_multiple_books():
     response = main.search('book title')
-    assert response.status_code == 200
+    assert 'items' in response.keys()
