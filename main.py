3558a042ab5697aa7e053a47fa620aad732c0511
Kylan Byrd <AvianAnalyst@gmail.com>
2020-04-17 23:49:50 -0700

    Parse returns list of dicts with each dict being an item

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from typing import List
 import os
 import requests
 import json
@@ -7,6 +8,11 @@ load_dotenv()
 SECRET = os.getenv('KEY')
 
 
-def search(query: str):
-    response = requests.get('https://www.googleapis.com/books/v1/volumes', {'q': query})
-    return json.loads(response.text)
+def search(query: str) -> str:
+    # TODO: obey the testing goat, but when im there use maxResults parameter to limit results
+    response = requests.get('https://www.googleapis.com/books/v1/volumes', {'q': query, 'key': SECRET})
+    return response.text
+
+
+def parse(param: str) -> List[dict]:
+    return json.loads(param)['items']
--- test_main.py
+++ test_main.py
@@ -3,4 +3,10 @@ import main
 
 def test_search_gets_valid_response_with_multiple_books():
     response = main.search('book title')
-    assert 'items' in response.keys()
+    assert '"items": [' in response
+
+
+def test_parser_returns_books():
+    results = main.parse(main.search('something'))
+    for item in results:
+        assert item['kind'] == 'books#volume'
