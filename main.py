2d09508f9155d89b36d8945e5f7c1872226021f4
Kylan Byrd <AvianAnalyst@gmail.com>
2020-04-21 16:37:55 -0700

    Reworked code into OOP model
    Reworking the code into a class will allow for cacheing search results in an attribute, and interfacing with the user through a REPL

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 from dotenv import load_dotenv
-from typing import List
+from typing import List, Union, Dict
 import os
 import requests
 import json
 
-load_dotenv()
-SECRET = os.getenv('KEY')
 
+class BookList:
+    def __init__(self) -> None:
+        load_dotenv()
+        self.SECRET: str = os.getenv('KEY')
+        self.search_results: Union[None, List[Dict[str, Union[str, List[str]]]]] = None
 
-def search(query: str) -> str:
-    response = requests.get('https://www.googleapis.com/books/v1/volumes',
-                            {'q': query, 'maxResults': 5, 'key': SECRET})
-    return response.text
+    def _search(self, query: str) -> str:
+        response = requests.get('https://www.googleapis.com/books/v1/volumes',
+                                {'q': query, 'maxResults': 5, 'key': self.SECRET})
+        return response.text
 
-
-def gather(query: str) -> List[dict]:
-    raw_book_json = search(query)
-    list_of_volume_info = [item['volumeInfo'] for item in json.loads(raw_book_json)['items']]
-    return [
-        {
-            field: value
-            for field, value in book.items()
-            if field in ['title', 'authors', 'publisher']
-        }
-        for book in list_of_volume_info
-    ]
-
-
-
+    def gather(self, query: str) -> None:
+        raw_book_json = self._search(query)
+        list_of_volume_info = [item['volumeInfo'] for item in json.loads(raw_book_json)['items']]
+        self.search_results = [
+            {
+                field: value
+                for field, value in book.items()
+                if field in ['title', 'authors', 'publisher']
+            }
+            for book in list_of_volume_info
+        ]
--- test_main.py
+++ test_main.py
@@ -1,20 +1,22 @@
-import main
+from main import BookList
 
 
 def test_search_gets_valid_response_with_multiple_books():
-    response = main.search('book title')
+    books = BookList()
+    response = books._search('book title')
     assert '"items": [' in response
 
 
 def test_gather_returns_no_more_than_5_items():
-    results = main.gather('t')
-    assert len(results) <= 5
+    books = BookList()
+    books.gather('t')
+    assert len(books.search_results) <= 5
 
 
 def test_gather_returns_title_author_publisher_for_each_result():
-    results = main.gather('the underland chronicles')
-    for book in results:
+    books = BookList()
+    books.gather('the underland chronicles')
+    for book in books.search_results:
         fields = book.keys()
         for field in ['authors', 'title', 'publisher']:
             assert field in fields
-
